10f169a5628ec41f49840e13a453beb6be1d780e
annguyen <annguyen.it.utc@gmail.com>
2022-10-08 23:39:52 +0700

    Update mapbox

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 
 /// App's home page
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   /// Default constructor
   const HomePage({super.key});
 
@@ -11,6 +11,13 @@ class HomePage extends StatelessWidget {
   static Route<void> route() {
     return MaterialPageRoute(builder: (_) => const HomePage());
   }
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final MapboxMapController _controller;
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +34,43 @@ class HomePage extends StatelessWidget {
           ),
           styleString:
               'mapbox://styles/snowflower1408/cl8ft6c6u000514oduxhorph6',
+          compassViewPosition: CompassViewPosition.TopRight,
+          annotationConsumeTapEvents: const [AnnotationType.symbol],
+          onMapCreated: (controller) {
+            _controller = controller;
+          },
+          onStyleLoadedCallback: () async {
+            await _controller.addSymbolLayer(
+              'cl8fqfj0p07fe29priw1jv79r',
+              'snowflower1408.cl8fqfj0p07fe29priw1jv79r-3hgi6',
+              const SymbolLayerProperties(),
+            );
+          },
+          onMapClick: (point, coordinates) async {
+            debugPrint('Test ----------------------');
+            debugPrint('$coordinates');
+            debugPrint('$point');
+
+            final rect = Rect.fromLTRB(
+              point.x - 100,
+              point.y - 100,
+              point.x + 100,
+              point.y + 100,
+            );
+            final featureList = await _controller.queryRenderedFeaturesInRect(
+              rect,
+              ['snowflower1408.cl8fqfj0p07fe29priw1jv79r-3hgi6'],
+              null,
+            );
+
+            debugPrint('${featureList.length}');
+
+            if (featureList.isNotEmpty) {
+              for (final feature in featureList) {
+                debugPrint(feature.toString());
+              }
+            }
+          },
         ),
       ),
     );
